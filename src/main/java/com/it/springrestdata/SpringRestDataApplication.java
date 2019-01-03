e38116a9c67c68bd3c924fb933c8351fc2a0519b
Rafael Reis <rafaelfc.ads@gmail.com>
2019-01-03 13:02:26 -0200

    first commit

---
+++ src/main/java/com/it/springrestdata/SpringRestDataApplication.java
@@ -0,0 +1,14 @@
+package com.it.springrestdata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRestDataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringRestDataApplication.class, args);
+	}
+
+}
+
---
+++ src/test/java/com/it/springrestdata/SpringRestDataApplicationTests.java
@@ -0,0 +1,17 @@
+package com.it.springrestdata;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringRestDataApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
