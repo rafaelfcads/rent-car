82edbe30d99ad106965042906a55a0a9ecf43357
Rafael Reis <rafaelfc.ads@gmail.com>
2019-01-03 15:14:26 -0200

    refactory entities

---
+++ src/main/java/com/it/springrestdata/vehicle/Vehicle.java
@@ -0,0 +1,37 @@
+package com.it.springrestdata.vehicle;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+
+@Entity
+@Data
+@EqualsAndHashCode(of = "id")
+@NoArgsConstructor
+@RequiredArgsConstructor
+public class Vehicle {
+	
+	@Id
+	@NonNull
+	private Long id;
+
+	@NonNull
+	private String name;
+	
+	@NonNull
+	private String brand;
+	
+	@NonNull
+	@Column(name = "manufacturing_year")
+	private int manufacturingYear;
+	
+	@NonNull
+	private float mileage;
+
+}
--- src/main/java/com/it/springrestdata/vehicle/VehicleRepository.java
+++ src/main/java/com/it/springrestdata/vehicle/VehicleRepository.java
@@ -1,7 +1,7 @@
-package com.it.springrestdata.airport;
+package com.it.springrestdata.vehicle;
 
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 @RepositoryRestResource
-public interface AirportRepository extends CrudRepository<Airport, String> {}
+public interface VehicleRepository extends CrudRepository<Vehicle, String> {}
