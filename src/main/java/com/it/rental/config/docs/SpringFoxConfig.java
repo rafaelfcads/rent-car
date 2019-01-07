dc68e6609e22108e5238b0e3fd57a00c00999583
Rafael Reis <rafaelfc.ads@gmail.com>
2019-01-07 19:17:39 -0200

    configure swagger

--- src/main/java/com/it/rental/config/docs/SpringFoxConfig.java
+++ src/main/java/com/it/rental/config/docs/SpringFoxConfig.java
@@ -1,10 +1,14 @@
 package com.it.rental.config.docs;
 
+import java.util.Collections;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -16,8 +20,22 @@ public class SpringFoxConfig {
     public Docket apiDocket() {
         return new Docket(DocumentationType.SWAGGER_2)
                 .select()
-                .apis(RequestHandlerSelectors.any())
-                .paths(PathSelectors.any())
-                .build();
+                .apis(RequestHandlerSelectors.basePackage("com.it.rental"))
+                .paths(PathSelectors.ant("/v2/**"))
+                .build()
+                .apiInfo(getApiInfo());
+    }
+    
+    private ApiInfo getApiInfo() {
+        return new ApiInfo(
+                "Spring Data REST - POSTGRESQL",
+                "Aplicando o guia de ref. Spring Data REST - POSTGRESQL",
+                "v1.0.0",
+                null,
+                null,
+                null,
+                null,
+                Collections.emptyList()
+        );
     }
 }
